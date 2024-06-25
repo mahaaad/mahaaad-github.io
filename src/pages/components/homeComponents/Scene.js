a3e7efbdfe6f84a915ba38360f1f667662648b09
mahaaad <mahad.rehan@gmail.com>
2024-06-25 03:41:06 -0400

    edited footer message

--- src/pages/components/homeComponents/Scene.js
+++ src/pages/components/homeComponents/Scene.js
@@ -4,7 +4,7 @@ import FadeIn from 'react-fade-in'
 function Scene(){
     return (
         <div>
-            <img className = "mahadvector" src = "/images/graphics/mahadvector2.svg" />
+            <img className = "mahadvector" src = "/images/graphics/mahadvector2024.svg" />
             <img className = "sidewalk" style = {{marginTop: "45.5vw", width: "100%",  boxShadow: "0px 5px 8px #00000030"}} src = "/images/graphics/sidewalk.svg" />
             <hr style = {{position: "absolute", top: "50vw", left: "5vw", width: "40%", opacity: "30%"}} />
             <img className = "arrowicon" src = "/images/icons/arrowicon.svg" />
